f89d036ce05ee5d01dca13695aa99fb8164bf2e5
fredy34009 <fredyantonioalfaro@gmail.com>
2024-09-24 21:18:11 -0600

    correccion

--- src/Producto.kt
+++ src/Producto.kt
@@ -1,3 +1,3 @@
-//clase producto recibe como parametros el nombre del producto, el precioy la cantidad
+//clase producto recibe como parametros el nombre del producto, el precio y la cantidad
 class Producto (val nombre:String, val precio:Double, var cantidad:Int) {
 }
